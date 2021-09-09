1466bddd8f6916282d45ffe0778d451ec632aafa
SUAREZGITHUB <culturalcatamarca@hotmail.com>
2021-09-09 13:55:20 -0300

    1.1

--- producto/read.php
+++ producto/read.php
@@ -3,9 +3,13 @@
     // aqui consumimos  la clase database
     // en VFP set procedure to "database.prg" ADDITIVE
      include_once '../config/database.php';
+     include_once '../utils/response.php';
      //en VFP $dbClass = newobjetc ("DBClass", "database.prg")
     $dbClass = new DBClass();
      //en VFP $conn.getConnection()
     $conn    = $dbClass->getConnection();
+
+    $res    = new Response();
+    echo json_encode($res->getResponse("success" ,null  ,200 , "conectado ok"));
 
 ?>
