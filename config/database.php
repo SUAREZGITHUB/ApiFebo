9405caf0136cb1d5324f10af9a5449a71130ede2
SUAREZGITHUB <culturalcatamarca@hotmail.com>
2021-09-09 00:33:33 -0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,1 +1,9 @@
-ppp
+<?php
+    // Almacena instancia de la conexion.*/
+    class DBClass{
+        private $conn;
+        // lo apunta al localhost que es nuestro ambito de desarrollo
+        private $host= 'localhost';
+    }
+
+?>
