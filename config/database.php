8ec78928179efde49300268c84f6d855aa70df4e
SUAREZGITHUB <culturalcatamarca@hotmail.com>
2021-09-09 01:11:05 -0300

    conexion

--- config/database.php
+++ config/database.php
@@ -1,9 +1,23 @@
 <?php
     // Almacena instancia de la conexion.*/
+    // Almacena instancia de la conexion
     class DBClass{
-        private $conn;
-        // lo apunta al localhost que es nuestro ambito de desarrollo
-        private $host= 'localhost';
+        private $conn; // 
+        // credenciales de acceso al servidor
+        private $host       = 'localhost';  // lo apunta al localhost que es nuestro ambito de desarrollo
+        private $dbName     = 'db_sistemafebo';
+        private $userName   = 'root';
+        private $pwd        = '';
+
+        // metodo que obtiene la instancia de la clase DBClass
+        public function getConnection(){
+            // le asigno mi cadena de conexion al PDO segun los parametros requeridos
+            $this->conn = new PDO('myql:host=' .$this->host. ';dbname=' .$this->dbname. ';',$this->username, $this->pwd);
+            $this->conn->exec("set names utf8");// que utilice la codificacion utf8
+            return $this->conn; // que vuelque y retorne todos los valores cargados a conn
+
+        }
+
     }
 
 ?>
