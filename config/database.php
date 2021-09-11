2cd6a5eb4072c57f5e49fd8d7e422e0d8489720c
SUAREZGITHUB <culturalcatamarca@hotmail.com>
2021-09-10 23:28:06 -0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -16,9 +16,8 @@
                 // le asigno mi cadena de conexion al objeto PDO segun los parametros requeridos
 
                 $this->conn = new PDO("mysql:host=" . $this->host . ";port=" .$this->port. ";dbname=" . $this->dbName . ";", $this->userName, $this->pwd);
-                //$this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->dbName . ";", $this->userName, $this->pwd);
                 $this->conn->exec("set names utf8");// que utilice la codificacion utf8
-                return $this->conn; // que vuelque y retorne todos los valores cargados a conn
+                return $this->conn; // retorna todos los valores cargados a $conn
             }catch(PDOException $ex){
                 echo $ex->getMessage();
             }
